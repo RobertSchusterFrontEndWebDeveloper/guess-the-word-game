0e4bfd633862ef997644a47466ae9c3752db9927
Rob Schuster <robert.is@icloud.com>
2023-02-24 20:41:34 -0500

    Add function to accept letter choice and validate choice, confirmation message also

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Global Variables:
-  // Letters guessed
-const guessedLetters = document.querySelector(".guessed-letters");
+  // Letters guessed element
+const guessedLettersElement = document.querySelector(".guessed-letters");
   // Guess button
 const guessLetterButton = document.querySelector(".guess");
   // Guessed letter current input
@@ -15,9 +15,13 @@ const remainingSpan = document.querySelector(".remaining span"); // **** look th
 const message = document.querySelector(".message");
   // Play again button
 const playAgain = document.querySelector(".play-again");
+
   // Random word
 const word = "magnolia";
+  // Letters guessed
+const guessedLetters = [];
 
+// Step #1
   // Setup placeholders for each letter of word to guess as "●"
     // const currentWord will be the current word to guess
 const currentWord = function (word) {
@@ -34,13 +38,44 @@ currentWord(word);
     // guessLetter will be the current guess input
 guessLetterButton.addEventListener("click", function(e) {
   e.preventDefault();
+  message.innerText = "";
   const letterChosen = currentGuess.value;
-  console.log(`${letterChosen} was just chose.`);
-     // clear the value for next guess input
+  // send to check if it is a single letter
+  const goodGuess = validateGuess(letterChosen);
+
+  if (goodGuess) {
+    makeGuess(letterChosen);
+  }
   currentGuess.value = "";
 });
 
+// Step #2
+  // function to accept value as parameter
+const validateGuess = function (letterChosen) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (letterChosen.length === 0 ) {
+    // to check if input was nothing
+    message.innerText = "You need to input a letter.";
+  } else if (letterChosen.length > 1) {
+    message.innerText = "Oops you entered 2 letters.";
+  } else if (!letterChosen.match(acceptedLetter)) {
+    // When input is other than letter
+    message.innerText = "Oh!  Something other than a letter was given. Try again!";
+  } else {
+    // single letter was input
+    return letterChosen;
+  }
+};
 
+const makeGuess = function(letterChosen) {
+  letterChosen = letterChosen.toUpperCase();
+  if(guessedLetters.includes(letterChosen)) {
+    message.innerText = "You Guessed this letter already :P  Try again."
+  } else {
+    guessedLetters.push(letterChosen);
+    console.log(guessedLetters);
+  }
+};
 
 
 
